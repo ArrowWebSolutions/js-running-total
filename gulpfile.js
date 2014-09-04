fe4b47b8873ab50eb1169aa461e2085377cdf43f
Arron King <arron.king@arrowdesign.co.uk>
2014-09-04 17:08:27 +0100

    Gulp file that can now test and then build the project.

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,16 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
+var karma = require('gulp-karma');
 
 var paths = {
   "scripts": "src/*.js"
 };
+
+var testFiles = [
+  "src/*.js",
+  "tests/*.js"
+];
 
 gulp.task('js', function(){
   return gulp.src(paths.scripts)
@@ -12,3 +18,16 @@ gulp.task('js', function(){
     .pipe(concat('js-running-total.min.js'))
     .pipe(gulp.dest('.'));
 });
+
+gulp.task('test', function(){
+  return gulp.src(testFiles)
+    .pipe(karma({
+      configFile: 'karma.conf.js',
+      action: 'run'
+    }))
+    .on('error', function(err){
+      throw err;
+    });
+});
+
+gulp.task('build', ['test', 'js']);

916d41c83b6743d33b2689a6d67efcf4c82f95ac
Arron King <arron.king@arrowdesign.co.uk>
2014-09-24 17:41:27 +0100

    More tests, then a percentage test

--- tests/Formatter.test.js
+++ tests/Formatter.test.js
@@ -36,10 +36,22 @@ describe("Format", function(){
     expect(Formatter.format(1000, 'number', {precision: 2})).toEqual('1,000.00');
     expect(Formatter.format(1000, 'number', {thousandsSeparator: '.', decimalPoint: ',', precision: 3})).toEqual('1.000,000');
     expect(Formatter.format(10000000, 'number')).toEqual('10,000,000');
+
+    //alternative format
+    expect(Formatter.number(10000)).toEqual('10,000');
   });
 
   it('should format currency', function() {
-    expect(Formatter.format(1000, 'currency', {precision: 2})).toEqual('£1,000.00');
+    expect(Formatter.format(1000, 'currency')).toEqual('£1,000.00');
+    expect(Formatter.format(1000, 'currency', {precision: 0})).toEqual('£1,000');
+    expect(Formatter.format(1000000, 'currency', {currencySymbol: '$'})).toEqual('$1,000,000.00');
+
+    //alternative format
+    expect(Formatter.currency(54321)).toEqual('£54,321.00');
+  });
+
+  it('should format percentage', function() {
+    expect(Formatter.format('0.1', 'percentage')).toEqual('10%');
   });
 
 });
